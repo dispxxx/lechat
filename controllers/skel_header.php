7262a7d87ac8b9f697b122850cfd1a19abf00afa
dispxxx <contact@dispix.net>
2015-11-23 15:46:35 +0100

    H1

--- controllers/skel_header.php
+++ controllers/skel_header.php
@@ -1,2 +1,14 @@
 <?php 
+
+$getArray = array('login', 'register', 'chat', 'private', 'dashboard');
+$headerArray = array('Log-in', 'Register', 'Public chatroom', 'Private discussion with', 'Dashboard');
+
+$key = array_search($page, $getArray);
+
+if (!is_nan($key))
+{	
+	$header = $headerArray[$key];
+}
+
+var_dump($header);
 require('./views/skel_header.phtml');
