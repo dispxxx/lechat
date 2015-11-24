01a701a202c25671d0129d88188cd31dd69cd9d9
Poloch0n <zjulien67@gmail.com>
2015-11-24 14:02:06 +0100

    design public

--- controllers/handler_public.php
+++ controllers/handler_public.php
@@ -7,12 +7,13 @@ if (isset($_POST['action']))
 	// Send action
 	if ($_POST['action'] == 'send')
 	{
-		if (isset($_POST['message_id_sender'], $_POST['message_content']))
+		if (isset($_POST['message_content']))
 		{
+			echo "hi";
 			$message_content = mysqli_real_escape_string($db, $_POST['message_content']);
 		
 			$query = "	INSERT INTO public(id_sender, content)
-						VALUES(".$_POST['message_id_sender'].", '".$message_content."')";
+						VALUES(".$_SESSION['id'].", '".$message_content."')";
 		
 			if($data = mysqli_query($db, $query))
 			{
