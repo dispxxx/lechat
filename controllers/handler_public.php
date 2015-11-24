d210e4670fb78eb75d95c9686596516b5b9c8584
dispxxx <contact@dispix.net>
2015-11-24 11:24:46 +0100

    aze

--- controllers/handler_public.php
+++ controllers/handler_public.php
@@ -1,2 +1,17 @@
 <?php 
-require('views/content_public.phtml');
+
+$query = '	SELECT id_sender, content, date_sent, user.name AS user_name
+			FROM public
+			LEFT JOIN user ON public.id_sender =  user.id
+			LIMIT 30';
+
+$data = mysqli_query($db, $query);
+
+while ($result = mysqli_fetch_assoc($data))
+{
+	echo '<p>';
+	echo '<em>'. date('G:i', strtotime($result['date_sent']))."</em> ";
+	echo "<strong>@".$result['user_name']."</strong>";
+	echo ': '.$result['content'];
+	echo '</p>';
+}
