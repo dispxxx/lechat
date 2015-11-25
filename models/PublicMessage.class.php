87f23ae00f2956de8691ce5a1d84d1b15e77418b
dispxxx <contact@dispix.net>
2015-11-25 16:59:28 +0100

    Le flinguage de cerveau putain

--- models/PublicMessage.class.php
+++ models/PublicMessage.class.php
@@ -30,7 +30,7 @@ class PublicMessage
 
 
 	// Setters
-	public function setMessage ($content)
+	public function setContent ($content)
 	{
 	
 		// Check content
---
+++ models/PublicMessageManager.class.php
@@ -0,0 +1,104 @@
+<?php
+require('models/PublicMessage.class.php');
+class PublicMessageManager
+{
+	private $db;
+
+	public function create ($content, User $sender)
+	{
+		$publicMessage = new PublicMessage();
+		$result = $publicMessage -> setContent($content);
+
+		if ($result === true)
+		{
+			$result = $publicMessage -> setSender($sender);
+
+			if ($result === true)
+			{
+				$content 	= mysqli_escape_string($this -> db, $publicMessage -> getContent());
+				$id_sender 	= $publicMessage -> getSender() -> getId();
+				$query 		= "	INSERT INTO message (content, id_sender)
+								VALUES('".$content."', '".$id_sender."')";
+				$result 	= mysqli_query($this -> db, $query);
+
+				if ($result)
+				{
+					$id = mysqli_insert_id($this -> db);
+
+					if ($id)
+					{
+						return $this -> readById($id);
+					}
+					else
+					{
+						return "Internal server error";
+					}
+				}
+				else
+				{
+					return mysqli_error($this -> db);
+				}
+			}
+			else
+			{
+				return $result;
+			}
+		}
+		else
+		{
+			return $result;
+		}
+	}
+
+	public function read ($id)
+	{
+		return $this -> findById($id);
+	}
+
+	public function readById ($id)
+	{
+		$id 	= intval($id);
+		$query 	= '	SELECT *
+					FROM message
+					WHERE id = "'. $id .'"';
+		$result	= mysqli_query($this -> db, $query);
+
+		if ($result)
+		{
+			$publicMessage = mysqli_fetch_object($result, "Message");
+
+			if ($publicMessage)
+			{
+				return $publicMessage;
+			}
+			else
+			{
+				return "Message not found";
+			}
+		}
+		else
+		{
+			return "Internal server error";
+		}
+	}
+
+	public function readByContent ($content)
+	{
+		
+	}
+
+	public function readByAuthor ($author)
+	{
+		
+	}
+
+	public function update (PublicMessage $message)
+	{
+
+	}
+
+	public function delete (PublicMessage $message)
+	{
+
+	}
+}
